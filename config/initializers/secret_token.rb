dacee9a39048f0f18f85a676d1eb9ca196d73bf7
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2013-12-16 14:55:45 -0500

    First Commit, Database Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicBlog::Application.config.secret_key_base = 'f8e526e5b30d7256039d9a78fa8d81839f4e1551d03d484d7b0601c03ca8d341c0b36b9cf7f0c0e59c5f450987d95cc4496ff16ac775f379418e318e6959c7c1'

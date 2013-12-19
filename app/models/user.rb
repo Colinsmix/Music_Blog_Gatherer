38ebf104c3ad54ff03464b50e7f491fc0274aed4
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2013-12-19 16:54:04 -0500

    Added Devise to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  acts_as_taggable
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   has_many :blogs

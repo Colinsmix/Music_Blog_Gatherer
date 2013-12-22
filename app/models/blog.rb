398b9309a5d9c010d1ac64c50dec2c085c006a08
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2013-12-22 15:46:21 -0500

    Added submitter association to Blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,8 @@ class Blog < ActiveRecord::Base
   acts_as_taggable
   acts_as_votable
   
-  belongs_to :user
+  belongs_to :submitter, class_name: 'User', foreign_key: :user_id
+
   has_many :comments, inverse_of: :blog
   has_many :favorites, inverse_of: :blog
 

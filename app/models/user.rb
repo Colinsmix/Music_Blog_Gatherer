68c5bb999f3edaf618b3cc76048bc6b6f2c27201
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-01-09 14:57:14 -0500

    Added favorite_blogs to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,18 @@ class User < ActiveRecord::Base
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  has_many :blogs, inverse_of: :submitter
-  has_many :favorites, inverse_of: :user
+  has_many :blogs, foreign_key: :submitter_id
+  has_many :favorites
+  # has_many :blogs, through: :favorites
   has_many :comments, inverse_of: :user
   
   validates_presence_of :email
   validates_format_of :email, :with => VALID_EMAIL_REGEX, :on => :create  
   validates_uniqueness_of :email
+
+  def favorite_blogs
+    favorites.map{|x| Blog.find(x.blog_id)}
+  end
 
 
 end

1b3e8c4c6ffa38cb257c6f5bab6a710e53490a00
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-01-12 13:28:59 -0500

    Comments Index and New Pages Created

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+
+  def index
+    @blog = Blog.find(params[:blog_id])
+    @comments = @blog.comments
+  end
+
+  def new
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.build
+  end
+
+  def create
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to blog_comments_path(@blog), notice: "Comment Posted Successfully!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
+  

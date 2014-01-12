2fb1862fd9fa0992d380266cbb469d50c3193eab
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-01-12 17:57:13 -0500

    Added url to Serializer

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,5 +1,5 @@
 class BlogSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :tag_list, :favorite_user
+  attributes :id, :name, :description, :tag_list, :favorite_user, :url
 
   def favorite_user
     if current_user

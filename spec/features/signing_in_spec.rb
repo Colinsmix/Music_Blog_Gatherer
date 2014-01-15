f34a439c0823eb82a44153c3befe2e29e5097c78
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-01-15 17:43:03 -0500

    Ran Tests

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -15,7 +15,7 @@ So that I can see my favorite blogs, vote on blogs, and submit a blog.) do
     scenario 'I see my profile page if signed in on an account without a name' do
       user.save!
       sign_in_as(user)
-      
+
       expect(page).to have_content("Welcome, #{user.email}")
     end
 
@@ -29,8 +29,8 @@ So that I can see my favorite blogs, vote on blogs, and submit a blog.) do
 
   end
 
-  context 'Forgetting your password' do 
-     
+  context 'Forgetting your password' do
+
     scenario "recover password" do
     visit root_path
     click_link 'Sign In'
--- spec/features/user_votes_on_blogs_spec.rb
+++ spec/features/user_votes_on_blogs_spec.rb
@@ -4,19 +4,17 @@ feature 'User votes on a Blog', %q(
 As a member
 I can up and downvote other blogs
 So that I have a way of expressing my opinion and recommending blogs to other users.) do
-  let!(:user){ FactoryGirl.build(:user) }
+  let!(:user){ FactoryGirl.create(:user) }
   let!(:blog1){ FactoryGirl.create(:blog, tag_list: ['Jazz', 'Rock'], status: 'Verified') }
 
-  scenario 'User enjoys a blog, and upvotes it to show his satisfaction' do
+  scenario 'User enjoys a blog, and upvotes it to show his satisfaction', js: true do
     visit root_path
-    user.save!
     sign_in_as(user)
-    click_link('Upvote')
+    click_on('Like')
 
-    expect(page).to_
-
-
+    expect(page).to have_content('1')
   end
+
 
   scenario 'User does not enjoy a blog, and downvotes it to show distaste' do
 

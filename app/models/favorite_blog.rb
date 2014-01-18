268787bb5845ab6fbbf0d26ab4022fd338dcec2b
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-01-18 14:57:21 -0500

    Favorite Blogs made unique

--- app/models/favorite_blog.rb
+++ app/models/favorite_blog.rb
@@ -1,5 +1,10 @@
 class FavoriteBlog < ActiveRecord::Base
   belongs_to :blog
   belongs_to :user
-  
+
+  validates_presence_of :user_id
+  validates_presence_of :blog_id
+
+  validates_uniqueness_of :user_id, scope: :blog_id
+
 end
